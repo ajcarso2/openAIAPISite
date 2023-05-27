5337aa2bf3839de89db4bdebe48187690d33a9f1
ajcarso2 <ajcarso2@asu.edu>
2023-05-26 20:24:49 -0700

    Got mark down to render, but can't get it to re render each message on a new line

--- js/chat.js
+++ js/chat.js
@@ -1,15 +1,30 @@
+var renderer = new marked.Renderer();
+renderer.paragraph = function(text) {
+  return text + '<br>';  // override paragraph rendering to avoid additional line breaks
+};
+
+marked.setOptions({
+  renderer: renderer
+});
+
+
 document.getElementById('chat-input').addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     event.preventDefault(); // Prevents the default action of the Enter key
     var input = this.value; // Gets the value of the input
     this.value = ''; // Clears the input
     var activeTextbox = document.querySelector('.giant-text-box.active'); // Find the active text box
+    console.log(activeTextbox);
+
+
     if (activeTextbox) { // If an active text box was found...
+      if(activeTextbox.textContent.trim() === "Content..."){
+        activeTextbox.innerHTML = "";
+      }
 
-      var output = "Your prompt was: " + input;
+      var output = marked.parse("**Your Prompt Was:** " + input); // Create a variable to store the output
 
-      // Render the output in markdown
-      activeTextbox.textContent = output;
+      activeTextbox.innerHTML += output; // Add the input value to the active text box
     }
   }
 });
